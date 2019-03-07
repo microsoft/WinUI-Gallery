4b35c756973832e733ae49223c0f9719cdfb08b4
Adam Barlow <abarlow@microsoft.com>
2019-03-06 19:17:58 -0800

    Creating a new XamlUICommand sample

---
+++ XamlControlsGallery/ControlPages/XamlUICommandPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace AppUIBasics.ControlPages
+{
+    public sealed partial class XamlUICommandPage : Page
+    {
+        public XamlUICommandPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CustomXamlUICommand_ExecuteRequested(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        {
+            //Insert Your Command Action Here
+        }
+        private void ControlExample_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+     }
+}
