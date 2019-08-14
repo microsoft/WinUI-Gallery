68ab222b6331802cf9e8febbfb23fd96de69a4b3
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-08-14 09:37:08 +0200

    Apply changes from PR

--- XamlControlsGallery/Navigation/NavigationRootPage.xaml.cs
+++ XamlControlsGallery/Navigation/NavigationRootPage.xaml.cs
@@ -167,7 +167,7 @@ namespace AppUIBasics
 
         private void OnNewControlsMenuItemLoaded(object sender, RoutedEventArgs e)
         {
-            if (IsFocusSupported && NavigationViewControl.IsPaneOpen)
+            if (IsFocusSupported && NavigationViewControl.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Expanded)
             {
                 controlsSearchBox.Focus(FocusState.Keyboard);
             }
