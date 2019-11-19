515ffa165b1595ce13fe1f9a48349c5e59c3f46e
SalmanMKC <13schishti@gmail.com>
2019-11-19 22:35:04 +0000

    changes undone

--- XamlControlsGallery/SamplePages/SampleCompactSizingPage.xaml.cs
+++ XamlControlsGallery/SamplePages/SampleCompactSizingPage.xaml.cs
@@ -17,11 +17,9 @@ namespace AppUIBasics.SamplePages
 {
     public sealed partial class SampleCompactSizingPage : Page
     {
-
         public SampleCompactSizingPage()
         {
             this.InitializeComponent();
         }
-
     }
 }
