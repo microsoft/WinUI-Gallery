581230f6c8644b7e9273fff4084b4d88019aa59d
Ana Wishnoff <anawish@microsoft.com>
2019-12-03 14:17:21 -0800

    Updated with PR feedback

--- XamlControlsGallery/ControlPages/ListViewPage.xaml.cs
+++ XamlControlsGallery/ControlPages/ListViewPage.xaml.cs
@@ -9,7 +9,6 @@
 //*********************************************************
 using AppUIBasics.Data;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
