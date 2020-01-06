527563380b1d003473480cced0026bf5acf3adcf
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-01-06 23:47:42 +0100

    PR feedback and code cleanup

--- XamlControlsGallery/ControlPages/InkToolbarPage.xaml.cs
+++ XamlControlsGallery/ControlPages/InkToolbarPage.xaml.cs
@@ -1,27 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
+﻿using Windows.UI.Core;
 using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AppUIBasics.ControlPages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class InkToolbarPage : Page
     {
         private InkPresenter _inkPresenter;
