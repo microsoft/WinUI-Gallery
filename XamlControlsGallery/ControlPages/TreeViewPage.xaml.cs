a1b54cb2a28bcad783251e3a636b09365a225b3a
Kevin Gallahan <kmgallahan@gmail.com>
2020-02-24 17:25:17 -0500

    Resolves 13 code style rule violations. (#359)
    * Resolves 6 violations of rule IDE0018.
    
    * Resolves 7 violations of rule IDE1005.

--- XamlControlsGallery/ControlPages/TreeViewPage.xaml.cs
+++ XamlControlsGallery/ControlPages/TreeViewPage.xaml.cs
@@ -222,10 +222,7 @@ namespace AppUIBasics.ControlPages
 
         private void NotifyPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
