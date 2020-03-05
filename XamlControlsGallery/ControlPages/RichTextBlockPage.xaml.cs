fab552b9a0f328e688638285d46d77428f8855eb
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-03-05 12:57:59 +0100

    Add texthighlighter RichTextBlock sample

--- XamlControlsGallery/ControlPages/RichTextBlockPage.xaml.cs
+++ XamlControlsGallery/ControlPages/RichTextBlockPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿//*********************************************************
+//*********************************************************
 //
 // Copyright (c) Microsoft. All rights reserved.
 // THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
@@ -14,10 +14,12 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -29,6 +31,38 @@ namespace AppUIBasics.ControlPages
         public RichTextBlockPage()
         {
             this.InitializeComponent();
+        }
+
+
+        private void HighlightColorCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get color to use
+            var selectedItem = (sender as ComboBox).SelectedItem as ComboBoxItem;
+            var color = Colors.Yellow;
+            switch (selectedItem.Content as string)
+            {
+                case "Yellow":
+                    color = Colors.Yellow;
+                    break;
+                case "Red":
+                    color = Colors.Red;
+                    break;
+                case "Blue":
+                    color = Colors.Blue;
+                    break;
+            }
+
+            // Get text
+            TextRange textRange = new TextRange() { StartIndex = 28, Length = 11 };
+            TextHighlighter highlighter = new TextHighlighter()
+            {
+                Background = new SolidColorBrush((Color)color),
+                Ranges = { textRange }
+            };
+
+
+            TextHighlightingRichTextBlock.TextHighlighters.Clear();
+            TextHighlightingRichTextBlock.TextHighlighters.Add(highlighter);
         }
     }
 }
