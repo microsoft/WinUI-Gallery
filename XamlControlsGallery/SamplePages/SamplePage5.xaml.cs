d0f1952f081502b24307854e1d636d6066813dcc
Ana Wishnoff <anawish@microsoft.com>
2020-04-24 17:56:12 -0700

    Adding Hierarchical NavigationView sample (#410)
    * Addded HierarchicalNavView Sample
    
    * Added sample code
    
    * Adding default radio button selection
    
    * Updating after PR review
    
    * Adding clarity for non-selectable parent and updating sample xaml
    
    * Clean up sample pages references
    
    Co-authored-by: Steven Moyes <stmoy@microsoft.com>

---
+++ XamlControlsGallery/SamplePages/SamplePage5.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace AppUIBasics.SamplePages
+{
+    public sealed partial class SamplePage5 : Page
+    {
+        public SamplePage5()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
