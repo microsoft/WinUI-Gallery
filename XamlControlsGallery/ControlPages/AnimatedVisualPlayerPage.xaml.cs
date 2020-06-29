a1b0d7d174d02ce1141a4b41a4d63bd628293b93
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-06-29 22:56:36 +0200

    Improve animated visual player demo

--- XamlControlsGallery/ControlPages/AnimatedVisualPlayerPage.xaml.cs
+++ XamlControlsGallery/ControlPages/AnimatedVisualPlayerPage.xaml.cs
@@ -1,41 +1,24 @@
-﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace AppUIBasics.ControlPages
 {
     public sealed partial class AnimatedVisualPlayerPage : Page
     {
+        private bool wasPaused = false;
+
         public AnimatedVisualPlayerPage()
         {
             this.InitializeComponent();
-        }
-
-        private void PlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Set forward playback rate.
-            // NOTE: This property is live, which means it takes effect even if the animation is playing.
-            Player.PlaybackRate = 1;
-            EnsurePlaying();
-        }
-
-        private void PauseButton_Checked(object sender, RoutedEventArgs e)
-        {
-            // Pause the animation, if playing.
-            // NOTE: Pausing does not cause PlayAsync to complete.
-            Player.Pause();
-        }
-
-        private void PauseButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            // Resume playing current animation.
-            Player.Resume();
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             // Stop the animation, which completes PlayAsync and resets to initial frame. 
             Player.Stop();
-            PauseButton.IsChecked = false;
+            Player.SetProgress(0);
+            SetIsPlayingIndicator(false);
+            wasPaused = false;
         }
 
         private void ReverseButton_Click(object sender, RoutedEventArgs e)
@@ -48,18 +31,56 @@ namespace AppUIBasics.ControlPages
 
         private void EnsurePlaying()
         {
-            if (PauseButton.IsChecked.Value)
+            if (!Player.IsPlaying)
             {
-                // Resume playing the animation, if paused.
-                PauseButton.IsChecked = false;
+                // Play the animation at the currently specified playback rate.
+                _ = Player.PlayAsync(fromProgress: 0, toProgress: 1, looped: false);
+                SetIsPlayingIndicator(true);
+            }
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Is the player playing and we did not get paused?
+            if(Player.IsPlaying && !wasPaused)
+            {
+                Player.Pause();
+                SetIsPlayingIndicator(false);
+                wasPaused = true;
             }
             else
             {
-                if (!Player.IsPlaying)
+                // Either not playing or paused
+                // If paused, resume and set paused to false
+                if(wasPaused)
                 {
-                    // Play the animation at the currently specified playback rate.
-                    _ = Player.PlayAsync(fromProgress: 0, toProgress: 1, looped: false);
+                    Player.Resume();
+                    wasPaused = false;
                 }
+                // Not playing, start animation now
+                else
+                {
+                    Player.PlaybackRate = 1;
+                    _ = Player.PlayAsync(fromProgress:0, toProgress: 1, looped: false);
+                    wasPaused = false;
+                }
+                SetIsPlayingIndicator(true);
+            }
+        }
+
+        private void SetIsPlayingIndicator(bool isPlaying)
+        {
+            if(isPlaying)
+            {
+                PlayIcon.Visibility = Visibility.Collapsed;
+                PauseIcon.Visibility = Visibility.Visible;
+                ToolTipService.SetToolTip(PlayPauseButton, "Pause");
+            }
+            else
+            {
+                PlayIcon.Visibility = Visibility.Visible;
+                PauseIcon.Visibility = Visibility.Collapsed;
+                ToolTipService.SetToolTip(PlayPauseButton, "Play");
             }
         }
     }
