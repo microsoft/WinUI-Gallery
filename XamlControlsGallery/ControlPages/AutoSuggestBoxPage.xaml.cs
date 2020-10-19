360d18c3029aaf3ad033ef5a8c22fc23ecd0db44
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-10-19 23:43:22 +0200

    Update AutoSuggestBox sample to be more illustrative (#565)
    * Update AuttoSuggestBox sample to be more illustrative
    
    * Update wording
    
    * Update sample code

--- XamlControlsGallery/ControlPages/AutoSuggestBoxPage.xaml.cs
+++ XamlControlsGallery/ControlPages/AutoSuggestBoxPage.xaml.cs
@@ -21,6 +21,111 @@ namespace AppUIBasics.ControlPages
 {
     public sealed partial class AutoSuggestBoxPage : Page
     {
+        private List<string> Cats = new List<string>()
+        {
+            "Abyssinian",
+            "Aegean",
+            "American Bobtail",
+            "American Curl",
+            "American Ringtail",
+            "American Shorthair",
+            "American Wirehair",
+            "Aphrodite Giant",
+            "Arabian Mau",
+            "Asian cat",
+            "Asian Semi-longhair",
+            "Australian Mist",
+            "Balinese",
+            "Bambino",
+            "Bengal",
+            "Birman",
+            "Bombay",
+            "Brazilian Shorthair",
+            "British Longhair",
+            "British Shorthair",
+            "Burmese",
+            "Burmilla",
+            "California Spangled",
+            "Chantilly-Tiffany",
+            "Chartreux",
+            "Chausie",
+            "Colorpoint Shorthair",
+            "Cornish Rex",
+            "Cymric",
+            "Cyprus",
+            "Devon Rex",
+            "Donskoy",
+            "Dragon Li",
+            "Dwelf",
+            "Egyptian Mau",
+            "European Shorthair",
+            "Exotic Shorthair",
+            "Foldex",
+            "German Rex",
+            "Havana Brown",
+            "Highlander",
+            "Himalayan",
+            "Japanese Bobtail",
+            "Javanese",
+            "Kanaani",
+            "Khao Manee",
+            "Kinkalow",
+            "Korat",
+            "Korean Bobtail",
+            "Korn Ja",
+            "Kurilian Bobtail",
+            "Lambkin",
+            "LaPerm",
+            "Lykoi",
+            "Maine Coon",
+            "Manx",
+            "Mekong Bobtail",
+            "Minskin",
+            "Napoleon",
+            "Munchkin",
+            "Nebelung",
+            "Norwegian Forest Cat",
+            "Ocicat",
+            "Ojos Azules",
+            "Oregon Rex",
+            "Oriental Bicolor",
+            "Oriental Longhair",
+            "Oriental Shorthair",
+            "Persian (modern)",
+            "Persian (traditional)",
+            "Peterbald",
+            "Pixie-bob",
+            "Ragamuffin",
+            "Ragdoll",
+            "Raas",
+            "Russian Blue",
+            "Russian White",
+            "Sam Sawet",
+            "Savannah",
+            "Scottish Fold",
+            "Selkirk Rex",
+            "Serengeti",
+            "Serrade Petit",
+            "Siamese",
+            "Siberian or´Siberian Forest Cat",
+            "Singapura",
+            "Snowshoe",
+            "Sokoke",
+            "Somali",
+            "Sphynx",
+            "Suphalak",
+            "Thai",
+            "Thai Lilac",
+            "Tonkinese",
+            "Toyger",
+            "Turkish Angora",
+            "Turkish Van",
+            "Turkish Vankedisi",
+            "Ukrainian Levkoy",
+            "Wila Krungthep",
+            "York Chocolate"
+        };
+
         public AutoSuggestBoxPage()
         {
             this.InitializeComponent();
@@ -28,16 +133,26 @@ namespace AppUIBasics.ControlPages
 
         private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            var suitableItems = new List<string>();
+            var splitText = sender.Text.ToLower().Split(" ");
+            foreach(var cat in Cats)
             {
-                List<string> suggestions = new List<string>()
+                var found = splitText.All((key)=>
                 {
-                    sender.Text + "1",
-                    sender.Text + "2"
-                };
-                Control1.ItemsSource = suggestions;
+                    return cat.ToLower().Contains(key);
+                });
+                if(found)
+                {
+                    suitableItems.Add(cat);
+                }
             }
+            if(suitableItems.Count == 0)
+            {
+                suitableItems.Add("No results found");
+            }
+            sender.ItemsSource = suitableItems;
         }
+
         private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
         {
             SuggestionOutput.Text = args.SelectedItem.ToString();
