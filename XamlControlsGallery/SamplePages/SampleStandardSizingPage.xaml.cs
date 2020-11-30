54e393c48c951872ff3a201b68ffaca0073b5cf4
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-11-30 02:44:02 +0100

    Fix issue with state being lost when switching between compact/standard sizing in standard sizing page (#521)

--- XamlControlsGallery/SamplePages/SampleStandardSizingPage.xaml.cs
+++ XamlControlsGallery/SamplePages/SampleStandardSizingPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,9 +17,24 @@ namespace AppUIBasics.SamplePages
 {
     public sealed partial class SampleStandardSizingPage : Page
     {
+        public TextBox FirstName => firstName;
+        public TextBox LastName => lastName;
+        public PasswordBox Password => password;
+        public PasswordBox ConfirmPassword => confirmPassword;
+        public DatePicker ChosenDate => chosenDate;
+
         public SampleStandardSizingPage()
         {
             this.InitializeComponent();
+        }
+
+        public void CopyState(SampleCompactSizingPage page)
+        {
+            FirstName.Text = page.FirstName.Text;
+            LastName.Text = page.LastName.Text;
+            Password.Password = page.Password.Password;
+            ConfirmPassword.Password = page.ConfirmPassword.Password;
+            ChosenDate.Date = page.ChosenDate.Date;
         }
     }
 }
