bbf5bc5d32f3a1eb4d979dee7233aef4d7339f07
MarissaMatt <62765959+MarissaMatt@users.noreply.github.com>
2021-04-02 16:09:11 -0700

    Made updates based on feedback.

--- XamlControlsGallery/ControlPages/IconElementPage.xaml.cs
+++ XamlControlsGallery/ControlPages/IconElementPage.xaml.cs
@@ -13,16 +13,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AppUIBasics.ControlPages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class IconControlsPage : Page
+    public sealed partial class IconElementPage : Page
     {
-        public IconControlsPage()
+        public IconElementPage()
         {
             this.InitializeComponent();
         }
