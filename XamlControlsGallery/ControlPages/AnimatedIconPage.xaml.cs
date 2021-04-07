7df29fc61354c28cc9d3190964c89e07cf886e42
MarissaMatt <62765959+MarissaMatt@users.noreply.github.com>
2021-04-07 13:53:43 -0700

    Adding AnimatedIcon and Icon Control Pages (#667)
    * Added AnimatedIcon and Icon Control Pages
    
    * Update XamlControlsGallery/DataModel/ControlInfoData.json
    
    Co-authored-by: Steven Moyes <stmoy@microsoft.com>
    
    * Made updates based on feedback.
    
    * Updates based on PR feedback.
    
    Co-authored-by: Steven Moyes <stmoy@microsoft.com>

---
+++ XamlControlsGallery/ControlPages/AnimatedIconPage.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AppUIBasics.ControlPages
+{
+    public sealed partial class AnimatedIconPage : Page
+    {
+        public AnimatedIconPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            AnimatedIcon.SetState(this.SearchAnimatedIcon, "PointerOver");
+        }
+
+        private void Button_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            AnimatedIcon.SetState(this.SearchAnimatedIcon, "Normal");
+        }
+    }
+}
