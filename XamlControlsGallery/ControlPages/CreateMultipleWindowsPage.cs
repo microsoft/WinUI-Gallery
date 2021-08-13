d854536af915a3b25459ecde83fa2ca1c7087b59
Tushar Rekhe <turekhe@microsoft.com>
2021-08-13 10:13:19 +0000

    XCG-Desktop - Add MultiWindow Page
    Adding new page for multiple desktop windows. Few things need updates/ attentions
    1. I am using default icon.
    2. Documentation and Related controls section are empty. Documentation section just points to msdn docs.
    3. There is one issue, the most recent window mirrors navigation on rest of windows. This is because XCG has many static data members that need to replaced with data from currently active window. Here is the task to track that item #35191854
    
    I have attached SS for references.
    
    Thank you![MTW_Page2.png](https://microsoft.visualstudio.com/f05f4dbf-3077-427d-91e0-b66d9e4ca374/_apis/git/repositories/7128736e-d71b-4e83-8e28-3f558a439611/pullRequests/6339095/attachments/MTW_Page2.png) ![MTW_page1.png](https://microsoft.visualstudio.com/f05f4dbf-3077-427d-91e0-b66d9e4ca374/_apis/git/repositories/7128736e-d71b-4e83-8e28-3f558a439611/pullRequests/6339095/attachments/MTW_page1.png)

---
+++ XamlControlsGallery/ControlPages/CreateMultipleWindowsPage.cs
@@ -0,0 +1,61 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+using AppUIBasics.Data;
+using System.Collections.Generic;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppUIBasics.ControlPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CreateMultipleWindowsPage : Page
+    {
+        private IEnumerable<ControlInfoDataGroup> _groups;
+
+        public CreateMultipleWindowsPage()
+        {
+            this.InitializeComponent();
+        }
+        public IEnumerable<ControlInfoDataGroup> Groups
+        {
+            get { return this._groups; }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _groups = ControlInfoDataSource.Instance.Groups;
+        }
+
+        private void List_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Control1.StartBringIntoView();
+        }
+
+        private void createNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Window();
+            var rootPage = new NavigationRootPage();
+            newWindow.Content = rootPage;
+            newWindow.Activate();
+
+            var targetPageType = typeof(NewControlsPage);
+            string targetPageArguments = string.Empty;
+            var rootFrame = (Frame)rootPage.FindName("rootFrame");
+            rootFrame.Navigate(targetPageType, targetPageArguments);
+        }
+    }
+}
