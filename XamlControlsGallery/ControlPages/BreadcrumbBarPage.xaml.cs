438e82b66c06de04012ce8266f1cc283ecebd9f7
Karen Lai <biilai@microsoft.com>
2021-09-27 15:43:52 -0700

    add dataTemplate + binding sample

--- XamlControlsGallery/ControlPages/BreadcrumbBarPage.xaml.cs
+++ XamlControlsGallery/ControlPages/BreadcrumbBarPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
 
 
@@ -6,10 +7,22 @@ namespace AppUIBasics.ControlPages
     public sealed partial class BreadcrumbBarPage : Page
     {
  
-    public BreadcrumbBarPage()
+        public BreadcrumbBarPage()
         {
             this.InitializeComponent();
             BreadcrumbBar1.ItemsSource = new string[] { "Home", "Documents", "Design", "Northwind", "Images", "Folder1", "Folder2", "Folder3" };
+
+            BreadcrumbBar2.ItemsSource = new List<Foo>
+            {
+                new Foo { Name = "Home"},
+                new Foo { Name = "Folder1" },
+                new Foo { Name = "Folder2" }
+            };
         }
+    }
+
+    public class Foo
+    {
+        public string Name { get; set; }
     }
 }
