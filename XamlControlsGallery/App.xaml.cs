7df7ca7555b28e03ad3c95eece5ede2fa4cfbe20
Marcel Wagner <marcel.alex.wagner@outlook.com>
2021-10-29 20:36:14 +0200

    Fix issue with suspending not restoring correctly (#547)
    * Fix issue with suspending not restoring correctly
    
    * CR feedback, small code cleanup

--- XamlControlsGallery/App.xaml.cs
+++ XamlControlsGallery/App.xaml.cs
@@ -1,4 +1,4 @@
-//*********************************************************
+﻿//*********************************************************
 //
 // Copyright (c) Microsoft. All rights reserved.
 // THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
@@ -87,18 +87,27 @@ namespace AppUIBasics
         }
 
 #if WINUI_PRERELEASE
-        private void App_Resuming(object sender, object e)
+        private async void App_Resuming(object sender, object e)
         {
-            switch (NavigationRootPage.RootFrame?.Content)
+            // We are being resumed, so lets restore our state!
+            try
             {
-                case ItemPage itemPage:
-                    itemPage.SetInitialVisuals();
-                    break;
-                case NewControlsPage newControlsPage:
-                case AllControlsPage allControlsPage:
-                    NavigationRootPage.Current.NavigationView.AlwaysShowHeader = false;
-                    break;
+                await SuspensionManager.RestoreAsync();
             }
+            finally
+            {
+                switch (NavigationRootPage.RootFrame?.Content)
+                {
+                    case ItemPage itemPage:
+                        itemPage.SetInitialVisuals();
+                        break;
+                    case NewControlsPage _:
+                    case AllControlsPage _:
+                        NavigationRootPage.Current.NavigationView.AlwaysShowHeader = false;
+                        break;
+                }
+            }
+
         }
 #endif
 
@@ -162,6 +171,25 @@ namespace AppUIBasics
 
             ThemeHelper.Initialize();
 
+            if (args.PreviousExecutionState == ApplicationExecutionState.Terminated
+                    || args.PreviousExecutionState == ApplicationExecutionState.Suspended)
+            {
+                try
+                {
+                    await SuspensionManager.RestoreAsync();
+                }
+                catch (SuspensionManagerException)
+                {
+                    //Something went wrong restoring state.
+                    //Assume there is no state and continue
+                }
+
+                Window.Current.Activate();
+
+                UpdateNavigationBasedOnSelectedPage(rootFrame);
+                return;
+            }
+
             Type targetPageType = typeof(NewControlsPage);
             string targetPageArguments = string.Empty;
 
@@ -169,19 +197,6 @@ namespace AppUIBasics
             {
                 if (args.Kind == ActivationKind.Launch)
                 {
-                    if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                    {
-                        try
-                        {
-                            await SuspensionManager.RestoreAsync();
-                        }
-                        catch (SuspensionManagerException)
-                        {
-                            //Something went wrong restoring state.
-                            //Assume there is no state and continue
-                        }
-                    }
-
                     targetPageArguments = ((Windows.ApplicationModel.Activation.LaunchActivatedEventArgs)args).Arguments;
                 }
                 else if (args.Kind == ActivationKind.Protocol)
@@ -242,6 +257,22 @@ namespace AppUIBasics
            CurrentWindow.Activate();
         }
 
+        private static void UpdateNavigationBasedOnSelectedPage(Frame rootFrame)
+        {
+            // Check if we brought back an ItemPage
+            if (rootFrame.Content is ItemPage itemPage)
+            {
+                // We did, so bring the selected item back into view
+                string name = itemPage.Item.Title;
+                if (Window.Current.Content is NavigationRootPage nav)
+                {
+                    // Finally brings back into view the correct item.
+                    // But first: Update page layout!
+                    nav.EnsureItemIsVisibleInNavigation(name);
+                }
+            }
+        }
+
         private Frame GetRootFrame()
         {
             Frame rootFrame;
@@ -290,6 +321,7 @@ namespace AppUIBasics
         {
             var deferral = e.SuspendingOperation.GetDeferral();
             await SuspensionManager.SaveAsync();
+            UpdateNavigationBasedOnSelectedPage(GetRootFrame());
             deferral.Complete();
         }
 #endif // WINUI_PRERELEASE
