468d2c22f4e8fa98574d98cb0866133502021dbd
bpulliam@microsoft.com <bpulliam@microsoft.com>
2022-01-22 16:24:30 -0500

    fix Windows.UI->Microsoft.UI in merges

--- XamlControlsGallery/ControlPages/NumberBoxPage.xaml.cs
+++ XamlControlsGallery/ControlPages/NumberBoxPage.xaml.cs
@@ -7,10 +7,9 @@
 // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 //*********************************************************
-using Microsoft.UI.Xaml.Controls;
 using Windows.Globalization.NumberFormatting;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace AppUIBasics.ControlPages
 {
