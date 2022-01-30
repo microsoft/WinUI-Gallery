6448f355c67ee4a6e88ae355a7d735ec4e39d6c2
bpulliam@microsoft.com <bpulliam@microsoft.com>
2022-01-29 22:24:55 -0500

    Windows->Microsoft, namespace

--- XamlControlsGallery/TabViewPages/MyTabContentControl.xaml.cs
+++ XamlControlsGallery/TabViewPages/MyTabContentControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
