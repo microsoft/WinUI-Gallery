c04bf34cd685dc400af412d1e8bd489b0e0c5b4c
Karen Lai <7976322+karenbtlai@users.noreply.github.com>
2022-03-21 20:15:23 -0600

    add subtitle

--- XamlControlsGallery/PageHeader.xaml.cs
+++ XamlControlsGallery/PageHeader.xaml.cs
@@ -38,6 +38,15 @@ namespace AppUIBasics
         public static readonly DependencyProperty TitleProperty = 
             DependencyProperty.Register("Title", typeof(object), typeof(PageHeader), new PropertyMetadata(null));
 
+        public object Subtitle
+        {
+            get { return GetValue(SubtitleProperty); }
+            set { SetValue(SubtitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubtitleProperty =
+            DependencyProperty.Register("Subtitle", typeof(object), typeof(PageHeader), new PropertyMetadata(null));
+
 
         public Thickness HeaderPadding
         {
