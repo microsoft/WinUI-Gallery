b4cd9b9e2f21a753a1a4a942f6c0e343be93429e
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2022-04-08 11:29:56 -0700

    Remove comment

--- XamlControlsGallery/TabViewPages/MyTabContentControl.xaml.cs
+++ XamlControlsGallery/TabViewPages/MyTabContentControl.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -12,7 +12,6 @@ namespace AppUIBasics.TabViewPages
             this.InitializeComponent();
 
 #if !UNIVERSAL
-            DesktopTextBlock.Visibility = Visibility.Visible;
             UwpTextBlock1.Visibility = Visibility.Collapsed;
             UwpTextBlock2.Visibility = Visibility.Collapsed;
 #endif
