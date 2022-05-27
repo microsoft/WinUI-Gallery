dd414b280696a82fe39554f2f90688e9c7d377af
Greg Woo <90726278+gregwoo-microsoft@users.noreply.github.com>
2022-05-27 10:14:19 -0700

    Fixed titlebar buttons when changing theme (#970)
    * Fixed titlebar buttons when changing theme
    
    * Created a helper class to simplify code
    
    * Removed ComImport to clean up code

---
+++ WinUIGallery/Helper/TitlebarHelper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppUIBasics.Helper;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Navigation;
+using WinRT;
+using System.Runtime.InteropServices;
+
+namespace WinUIGallery.DesktopWap.Helper
+{
+    internal class TitlebarHelper
+    {
+
+        public static void triggerTitleBarRepaint()
+        {
+            // to trigger repaint tracking task id 38044406
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(AppUIBasics.App.StartupWindow);
+            var activeWindow = AppUIBasics.Win32.GetActiveWindow();
+            if (hwnd == activeWindow)
+            {
+                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_INACTIVE, IntPtr.Zero);
+                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_ACTIVE, IntPtr.Zero);
+            }
+            else
+            {
+                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_ACTIVE, IntPtr.Zero);
+                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_INACTIVE, IntPtr.Zero);
+            }
+
+        }
+
+    }
+}
