3f00402e4a150f001873b9bec4aa904f32656c4b
Kristen Schau <krschau@microsoft.com>
2022-07-14 15:00:15 -0700

    update to 2.8, add WebView2 content

---
+++ WinUIGallery/ControlPages/WebView2Page.xaml.cs
@@ -0,0 +1,22 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace AppUIBasics.ControlPages
+{
+    public sealed partial class WebView2Page : Page
+    {
+        public WebView2Page()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
