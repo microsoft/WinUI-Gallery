2d9edf484ac0f1ad1543e442c36ced24bff5212b
Scott Jones <sjones@microsoft.com>
2023-01-24 13:55:33 -0800

    Use assembly version instead of package version, to prevent crash on unpackaged app settings

--- WinUIGallery/Properties/AssemblyInfo.cs
+++ WinUIGallery/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppUIBasics")]
+[assembly: AssemblyTitle("WinUIGallery")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AppUIBasics")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("WinUIGallery")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Note: package version and assembly version must be kept in sync
+[assembly: AssemblyVersion("1.3.16.0")]
+[assembly: AssemblyFileVersion("1.3.16.0")]
 [assembly: ComVisible(false)]
