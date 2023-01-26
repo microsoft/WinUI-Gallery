bdc770fe11c795bb384f3234d07edfd70f14cf6f
Scott Jones <scottj1s@gmail.com>
2023-01-26 07:18:01 -0800

    Use assembly version instead of package version, to prevent crash on unpackaged app settings (#1175)
    * Use assembly version instead of package version, to prevent crash on unpackaged app settings
    
    * Update versions
    
    * unintended add

--- WinUIGallery/SettingsPage.xaml.cs
+++ WinUIGallery/SettingsPage.xaml.cs
@@ -33,7 +33,7 @@ namespace AppUIBasics
         {
             get
             {
-                var version = Windows.ApplicationModel.Package.Current.Id.Version;
+                var version = System.Reflection.Assembly.GetEntryAssembly().GetName().Version;
                 return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
             }
         }
