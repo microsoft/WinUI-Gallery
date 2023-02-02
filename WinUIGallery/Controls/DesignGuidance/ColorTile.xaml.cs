adeb9d2c062549a8c0235eb2be2a358d3c33584f
Marcel Wagner <marcel.alex.wagner@outlook.com>
2023-02-02 20:16:50 +0100

    Implement discussed changes

--- WinUIGallery/Controls/DesignGuidance/ColorTile.xaml.cs
+++ WinUIGallery/Controls/DesignGuidance/ColorTile.xaml.cs
@@ -1,24 +1,10 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using Windows.ApplicationModel.DataTransfer;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace WinUIGallery.DesktopWap.Controls.DesignGuidance
 {
@@ -58,6 +44,22 @@ namespace WinUIGallery.DesktopWap.Controls.DesignGuidance
         public static readonly DependencyProperty ColorValueProperty =
             DependencyProperty.Register("ColorValue", typeof(string), typeof(ColorTile), new PropertyMetadata(""));
 
+
+
+        public bool ShowSeparator
+        {
+            get { return (bool)GetValue(ShowSeparatorProperty); }
+            set { SetValue(ShowSeparatorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShowSeparator.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowSeparatorProperty =
+            DependencyProperty.Register("ShowSeparator", typeof(bool), typeof(ColorTile), new PropertyMetadata(true));
+
+
+
+
+
         public ColorTile()
         {
             this.InitializeComponent();
@@ -69,6 +71,16 @@ namespace WinUIGallery.DesktopWap.Controls.DesignGuidance
             package.SetText(ColorBrushName);
             Clipboard.SetContent(package);
 
+        }
+
+        private void UserControl_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            CopyBrushNameButton.Visibility = Visibility.Visible;
+        }
+
+        private void UserControl_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            CopyBrushNameButton.Visibility = Visibility.Collapsed;
         }
     }
 }
