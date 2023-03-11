3bffcf5fd720ed62417f89de4b57ac49f67e193b
Niels Laute <niels.laute@live.nl>
2023-03-11 15:57:40 +0100

    Using segmented control

--- WinUIGallery/ControlPages/DesignGuidance/ColorsPage.xaml.cs
+++ WinUIGallery/ControlPages/DesignGuidance/ColorsPage.xaml.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 using WinUIGallery.DesktopWap.Controls.DesignGuidance.ColorSections;
 
 namespace AppUIBasics.ControlPages
@@ -12,41 +11,34 @@ namespace AppUIBasics.ControlPages
         public ColorsPage()
         {
             this.InitializeComponent();
-            NavigationFrame.Navigate(typeof(TextSection));
+        }
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (PageSelector.SelectedIndex)
+            {
+                case 0:
+                    NavigationFrame.Navigate(typeof(TextSection));
+                    break;
+                case 1:
+                    NavigationFrame.Navigate(typeof(FillSection));
+                    break;
+                case 2:
+                    NavigationFrame.Navigate(typeof(StrokeSection));
+                    break;
+                case 3:
+                    NavigationFrame.Navigate(typeof(BackgroundSection));
+                    break;
+                case 4:
+                    NavigationFrame.Navigate(typeof(SignalSection));
+                    break;
+            }
         }
 
-        private void ToggleButton_Checked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        private void Page_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
         {
-            if(sender is ToggleButton item)
-            {
-                switch (item.Content)
-                {
-                    case "Text":
-                        NavigationFrame.Navigate(typeof(TextSection));
-                        break;
-                    case "Fill":
-                        NavigationFrame.Navigate(typeof(FillSection));
-                        break;
-                    case "Stroke":
-                        NavigationFrame.Navigate(typeof(StrokeSection));
-                        break;
-                    case "Background":
-                        NavigationFrame.Navigate(typeof(BackgroundSection));
-                        break;
-                    case "Signal":
-                        NavigationFrame.Navigate(typeof(SignalSection));
-                        break;
-                }
-            }
-            foreach(var child in SelectionElement.Children)
-            {
-                if (child != sender)
-                {
-                    if (child is ToggleButton button)
-
-                        button.IsChecked = false;
-                }
-            }
+            PageSelector.SelectionChanged -= OnSelectionChanged;
+            NavigationFrame.Navigate(typeof(TextSection));
+            PageSelector.SelectionChanged += OnSelectionChanged;
         }
     }
 }
