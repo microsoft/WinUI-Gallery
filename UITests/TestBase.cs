5488474b98343d451e891b939dbb4dacac3aaae0
Marcel Wagner <marcel.alex.wagner@outlook.com>
2023-04-26 18:03:34 +0200

    Add axe testing, add license headers

--- UITests/TestBase.cs
+++ UITests/TestBase.cs
@@ -1,5 +1,19 @@
-﻿using Microsoft.VisualBasic;
-using OpenQA.Selenium.Appium.Windows;
+﻿//******************************************************************************
+//
+// Copyright (c) 2023 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
 using OpenQA.Selenium.Interactions;
 using System.Threading;
 
@@ -17,6 +31,8 @@ namespace UITests
 			search.SendKeys(name);
 			Thread.Sleep(1_000);
 			Session.FindElementByName(name).Click();
+
+			AxeHelper.AssertNoAccessibilityErrors();
 		}
 
 		public static void TypeText(string text)
