8538b70337a42d87dadace9c60b763a109c2136e
Karen Lai <biilai@microsoft.com>
2023-09-22 16:02:23 -0700

    fix build error

--- UITests/Tests/AxeScanAllTests.cs
+++ UITests/Tests/AxeScanAllTests.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
-using Windows.Storage;
 using System.IO;
 using System.Collections.ObjectModel;
 
