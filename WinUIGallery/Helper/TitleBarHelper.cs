d3561e2e4d89414eb574cd5ce4012b9eaae8ef04
Pratik Anand <pratik.preet@gmail.com>
2023-10-03 12:29:10 -0700

    Clickable Interactive control sample for custom titlebar (#1360)
    With 1.4 changes, WinUI 3 custom titlebar now uses appwindow titlebar + nonclientinputpointersource apis under the hood. This opens up new possibilities like allowing clickable interactive controls like textbox, button in the titlebar area, surrounded by draggable region on both left and the right sides.
    
    This code adds a sample to titlebar page which shows users how to create interactive controls in winui 3 titlebar. It also demonstrates the power of mixing and matching high level winui 3 custom titlebar apis and low level nonclient apis.

--- WinUIGallery/Helper/TitleBarHelper.cs
+++ WinUIGallery/Helper/TitleBarHelper.cs
@@ -19,27 +19,11 @@ using AppUIBasics;
 
 namespace WinUIGallery.DesktopWap.Helper
 {
+
     internal class TitleBarHelper
     {
-
-        private static void triggerTitleBarRepaint(Window window)
-        {
-            // to trigger repaint tracking task id 38044406
-            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-            var activeWindow = AppUIBasics.Win32.GetActiveWindow();
-            if (hwnd == activeWindow)
-            {
-                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_INACTIVE, IntPtr.Zero);
-                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_ACTIVE, IntPtr.Zero);
-            }
-            else
-            {
-                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_ACTIVE, IntPtr.Zero);
-                AppUIBasics.Win32.SendMessage(hwnd, AppUIBasics.Win32.WM_ACTIVATE, AppUIBasics.Win32.WA_INACTIVE, IntPtr.Zero);
-            }
-
-        }
-
+        // workaround as Appwindow titlebar doesn't update caption button colors correctly when changed while app is running
+        // https://task.ms/44172495
         public static Windows.UI.Color ApplySystemThemeToCaptionButtons(Window window)
         {
             var res = Application.Current.Resources;
@@ -61,7 +45,7 @@ namespace WinUIGallery.DesktopWap.Helper
         {
             var res = Application.Current.Resources;
             res["WindowCaptionForeground"] = color;
-            triggerTitleBarRepaint(window);
+            window.AppWindow.TitleBar.ButtonForegroundColor = color;
         }
     }
 }
