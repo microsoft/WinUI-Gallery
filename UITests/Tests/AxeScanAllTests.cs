0df5a378c24e116c863c69d86a71c2878835aa1d
Karen Lai <biilai@microsoft.com>
2023-10-11 11:25:23 -0700

    refactor to parse json

--- UITests/Tests/AxeScanAllTests.cs
+++ UITests/Tests/AxeScanAllTests.cs
@@ -10,13 +10,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
 using System.Reflection;
+using Newtonsoft.Json;
 
 namespace UITests.Tests
 {
     [TestClass]
     public class AxeScanAll : TestBase
     {       
-        public static readonly string xmlUri = "WinUIGalleryTestData.xml";
+        public static readonly string jsonUri = "ControlInfoData.json";
         public static new WindowsDriver<WindowsElement> Session => SessionManager.Session;
 
         public static string[] ExclusionList =
@@ -25,26 +26,43 @@ namespace UITests.Tests
             "Icons" // https://github.com/CommunityToolkit/Windows/issues/240 External toolkit SettingsExpander does not pass Axe testing
         };
 
+        public class ControlInfoData
+        {
+            public List<Group> Groups { get; set; }
+        }
+
+        public class Group
+        {
+            [JsonProperty("UniqueId")]
+            public string UniqueId { get; set; }
+          
+            [JsonProperty("Items")]
+            public List<Item> Items { get; set; }
+        }
+
+        public class Item
+        {
+            [JsonProperty("UniqueId")]
+            public string UniqueId { get; set; }
+        }
+
         private static IEnumerable<object[]> TestData()
         {
             var testCases = new List<object[]>();
 
-            // Load the XML file
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load(xmlUri);
+            string jsonContent = System.IO.File.ReadAllText(jsonUri);
+            var controlInfoData = JsonConvert.DeserializeObject<ControlInfoData>(jsonContent);
 
-            // Select all tables in the XML
-            var tables = xmlDoc.SelectNodes("//Table");
-            foreach (XmlNode tableNode in tables)
+            foreach (var group in controlInfoData.Groups)
             {
-                var sectionName = tableNode.Attributes["Id"].Value;
+                var sectionName = group.UniqueId;
 
                 // Select all row names within the current table
-                var rows = tableNode.SelectNodes("Row");
+                var items = group.Items;
 
-                foreach (XmlNode rowNode in rows)
+                foreach (var item in items)
                 {
-                    var pageName = rowNode.Attributes["Name"].Value;
+                    var pageName = item.UniqueId;
 
                     // Skip pages in the exclusion list.
                     if (ExclusionList.Contains(pageName))
