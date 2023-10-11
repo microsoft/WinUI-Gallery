9a2c60b4a5f6fa39bb0d63cabb42976f9511dd27
Karen <7976322+karkarl@users.noreply.github.com>
2023-10-11 12:32:02 -0700

    Add Test to Scan All Pages for Axe Issues (#1361)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    - Add a test that physically navigates to each page via NavView and
    check for Axe issues.
    - Utilizes DynamicData attribute to create grouped tests based on
    section category (ie. TreeView parent)
    - This also tests for run-time crashes with each page. 
    - Miscellaneous Axe fixes.
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)

--- UITests/AxeHelper.cs
+++ UITests/AxeHelper.cs
@@ -45,7 +45,8 @@ namespace UITests
 				.Where(rule => rule.Rule.ID != RuleId.SiblingUniqueAndFocusable);
             if (testResult.Any())
 			{
-				var mappedResult = testResult.Select(result => "Element " + result.Element.Properties["ControlType"] + " violated rule '" + result.Rule.Description + "'.");
+				var mappedResult = testResult.Select(result =>
+				"Element " + result.Element.Properties["ControlType"] + " violated rule '" + result.Rule.Description + "'.");
 				Assert.Fail("Failed with the following accessibility errors \r\n" + string.Join("\r\n", mappedResult));
 			}
 		}
