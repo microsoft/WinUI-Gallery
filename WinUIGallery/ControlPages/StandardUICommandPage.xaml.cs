a449b4a77ea5d128db0ccdfa5c0d6ebb42eeb66c
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2023-10-13 17:46:26 -0700

    add tostring to view model so accessibility name does not default to type name

--- WinUIGallery/ControlPages/StandardUICommandPage.xaml.cs
+++ WinUIGallery/ControlPages/StandardUICommandPage.xaml.cs
@@ -13,6 +13,11 @@ namespace AppUIBasics.ControlPages
     {
         public string Text { get; set; }
         public ICommand Command { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
     }
 
     public sealed partial class StandardUICommandPage : Page
