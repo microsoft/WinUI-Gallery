defc3ead499257290c7ef202d111f4e3db3ef240
Ben Stolovitz <ben@stolovitz.com>
2024-01-12 14:01:13 -0500

    list lots of styles!

--- WinUIGallery/ControlPages/ButtonPage.xaml.cs
+++ WinUIGallery/ControlPages/ButtonPage.xaml.cs
@@ -7,16 +7,43 @@
 // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 //*********************************************************
+using System.Linq;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 
 namespace AppUIBasics.ControlPages
 {
+    public sealed class ButtonResource
+    {
+        public string Name;
+        public SolidColorBrush Brush;
+    }
+
     public sealed partial class ButtonPage : Page
     {
         public ButtonPage()
         {
             this.InitializeComponent();
+
+            var buttonResources = Application.Current.Resources.MergedDictionaries
+                .SelectMany((d) =>
+                {
+                    return d.ThemeDictionaries.SelectMany((t) =>
+                    {
+                        return (t.Value as ResourceDictionary).Where((r) => r.Key.ToString().StartsWith("Button"));
+                    });
+                })
+                .Where(r => r.Value is SolidColorBrush)
+                .Select((r) =>
+                {
+                    return new ButtonResource()
+                    {
+                        Name = r.Key.ToString(),
+                        Brush = r.Value as SolidColorBrush
+                    };
+                });
+            LightweightStylesBox.ItemsSource = buttonResources;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
