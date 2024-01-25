1978c18dbe47ae0db614e0d4e65a9a481e1d2497
Aaron Junker-Wildi <aaron.junker@outlook.com>
2024-01-25 17:02:59 +0000

    Update copyright year and copyright string

--- WinUIGallery/Properties/AssemblyInfo.cs
+++ WinUIGallery/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinUIGallery")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
