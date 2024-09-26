ebe5fab3e1330fae3264282bfa6e6b76d0249bd1
Luke Longley <18177025+llongley@users.noreply.github.com>
2024-09-26 16:02:51 -0700

    Fixing tabs.

--- UITests/SessionManager.cs
+++ UITests/SessionManager.cs
@@ -24,129 +24,129 @@ using System.Threading;
 
 namespace UITests
 {
-	[TestClass]
-	public class SessionManager
-	{
-		private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
-		private static readonly string[] WinUIGalleryAppIDs = new string[]{
-			// WinUI 3 Gallery apps built in the lab
+    [TestClass]
+    public class SessionManager
+    {
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private static readonly string[] WinUIGalleryAppIDs = new string[]{
+            // WinUI 3 Gallery apps built in the lab
             "Microsoft.WinUI3ControlsGallery.Debug_grv3cx5qrw0gp!App",
             "Microsoft.WinUI3ControlsGallery_grv3cx5qrw0gp!App",
-			// WinUI 3 Gallery apps built locally
+            // WinUI 3 Gallery apps built locally
             "Microsoft.WinUI3ControlsGallery.Debug_8wekyb3d8bbwe!App",
             "Microsoft.WinUI3ControlsGallery_8wekyb3d8bbwe!App"
         };
 
-		private static uint appIdIndex = 0;
+        private static uint appIdIndex = 0;
 
-		private static WindowsDriver<WindowsElement> _session;
-		public static WindowsDriver<WindowsElement> Session
-		{
-			get
-			{
-				if (_session is null)
-				{
-					Setup(null);
-				}
-				return _session;
-			}
-		}
+        private static WindowsDriver<WindowsElement> _session;
+        public static WindowsDriver<WindowsElement> Session
+        {
+            get
+            {
+                if (_session is null)
+                {
+                    Setup(null);
+                }
+                return _session;
+            }
+        }
 
-		[AssemblyInitialize]
-		public static void Setup(TestContext _)
-		{
-			if (_session is null)
-			{
+        [AssemblyInitialize]
+        public static void Setup(TestContext _)
+        {
+            if (_session is null)
+            {
 
-				int timeoutCount = 50;
+                int timeoutCount = 50;
 
-				TryInitializeSession();
-				if (_session is null)
-				{
+                TryInitializeSession();
+                if (_session is null)
+                {
                     // WinAppDriver is probably not running, so lets start it!
                     string winAppDriverX64Path = Path.Join(Environment.GetEnvironmentVariable("ProgramFiles"), "Windows Application Driver", "WinAppDriver.exe");
                     string winAppDriverX86Path = Path.Join(Environment.GetEnvironmentVariable("ProgramFiles(x86)"), "Windows Application Driver", "WinAppDriver.exe");
 
                     if (File.Exists(winAppDriverX64Path))
-					{
-						Process.Start(winAppDriverX64Path);
-					}
-					else if (File.Exists(winAppDriverX86Path))
-					{
-						Process.Start(winAppDriverX86Path);
-					}
-					else
-					{
-						throw new Exception("Unable to start WinAppDriver since no suitable location was found.");
-					}
+                    {
+                        Process.Start(winAppDriverX64Path);
+                    }
+                    else if (File.Exists(winAppDriverX86Path))
+                    {
+                        Process.Start(winAppDriverX86Path);
+                    }
+                    else
+                    {
+                        throw new Exception("Unable to start WinAppDriver since no suitable location was found.");
+                    }
 
-					Thread.Sleep(10000);
-					TryInitializeSession();
-				}
+                    Thread.Sleep(10000);
+                    TryInitializeSession();
+                }
 
-				while (_session is null && timeoutCount < 1000 * 4)
-				{
-					TryInitializeSession();
-					Thread.Sleep(timeoutCount);
-					timeoutCount *= 2;
-				}
+                while (_session is null && timeoutCount < 1000 * 4)
+                {
+                    TryInitializeSession();
+                    Thread.Sleep(timeoutCount);
+                    timeoutCount *= 2;
+                }
 
-				Thread.Sleep(3000);
-				Assert.IsNotNull(_session);
-				Assert.IsNotNull(_session.SessionId);
-				AxeHelper.InitializeAxe();
-				
-				// Dismiss the disclaimer window that may pop up on the very first application launch
-				// If the disclaimer is not found, this throws an exception, so lets catch that
-				try
-				{
-					_session.FindElementByName("Disclaimer").FindElementByName("Accept").Click();
-				}
-				catch (OpenQA.Selenium.WebDriverException) { }
+                Thread.Sleep(3000);
+                Assert.IsNotNull(_session);
+                Assert.IsNotNull(_session.SessionId);
+                AxeHelper.InitializeAxe();
+                
+                // Dismiss the disclaimer window that may pop up on the very first application launch
+                // If the disclaimer is not found, this throws an exception, so lets catch that
+                try
+                {
+                    _session.FindElementByName("Disclaimer").FindElementByName("Accept").Click();
+                }
+                catch (OpenQA.Selenium.WebDriverException) { }
 
-				// Wait if something is still animating in the visual tree
-				_session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-				_session.Manage().Window.Maximize();
-			}
-		}
+                // Wait if something is still animating in the visual tree
+                _session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                _session.Manage().Window.Maximize();
+            }
+        }
 
-		[AssemblyCleanup()]
-		public static void TestRunTearDown()
-		{
-			TearDown();
-		}
+        [AssemblyCleanup()]
+        public static void TestRunTearDown()
+        {
+            TearDown();
+        }
 
-		public static void TearDown()
-		{
-			if (_session is not null)
-			{
-				_session.CloseApp();
-				_session.Quit();
-				_session = null;
-			}
-		}
+        public static void TearDown()
+        {
+            if (_session is not null)
+            {
+                _session.CloseApp();
+                _session.Quit();
+                _session = null;
+            }
+        }
 
-		private static void TryInitializeSession()
-		{
-			AppiumOptions appiumOptions = new AppiumOptions();
-			appiumOptions.AddAdditionalCapability("app", WinUIGalleryAppIDs[appIdIndex]);
-			appiumOptions.AddAdditionalCapability("deviceName", "WindowsPC");
-			try
-			{
-				_session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumOptions);
-			}
-			catch (OpenQA.Selenium.WebDriverException exc)
-			{
-				// Use next app ID since the current one was failing
-				if (exc.Message.Contains("Package was not found"))
-				{
-					appIdIndex++;
-				}
-				else
-				{
-					Console.WriteLine("Failed to update start driver, got exception:" + exc.Message);
-				}
-			}
-		}
-	}
+        private static void TryInitializeSession()
+        {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", WinUIGalleryAppIDs[appIdIndex]);
+            appiumOptions.AddAdditionalCapability("deviceName", "WindowsPC");
+            try
+            {
+                _session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumOptions);
+            }
+            catch (OpenQA.Selenium.WebDriverException exc)
+            {
+                // Use next app ID since the current one was failing
+                if (exc.Message.Contains("Package was not found"))
+                {
+                    appIdIndex++;
+                }
+                else
+                {
+                    Console.WriteLine("Failed to update start driver, got exception:" + exc.Message);
+                }
+            }
+        }
+    }
 }
