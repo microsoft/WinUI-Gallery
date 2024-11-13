e2cf2d029e534c9e4ec0ad11b3ce7ac5fa871215
Geoffrey Trousdale <getrou@microsoft.com>
2024-11-13 16:52:49 -0500

    Add first steps for VisualCapture

--- WinUIGallery/Shaders/CaptureHelper.cs
+++ WinUIGallery/Shaders/CaptureHelper.cs
@@ -1,6 +1,10 @@
+using Microsoft.Graphics.Canvas.UI.Composition;
+using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Display;
+using Microsoft.UI.Composition;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
@@ -12,6 +16,8 @@ using Windows.Foundation;
 using Windows.Graphics.Imaging;
 using Windows.Storage.Pickers;
 using WinUIGallery.Helper;
+using Windows.Graphics;
+using WinRT;
 
 namespace WinUIGallery.Shaders
 {
@@ -61,6 +67,23 @@ namespace WinUIGallery.Shaders
             await renderTarget.RenderAsync(dialogContent, (int)(transformedBounds.Width / dpi), (int)(transformedBounds.Height / dpi));
 
             return transformedBounds;
+        }
+
+        public static async void CaptureTo2(this UIElement element)
+        {
+            Visual backingVisual = ElementCompositionPreview.GetElementVisual(element);
+            var compositor = backingVisual.Compositor;
+            var canvasDevice = CanvasDevice.GetSharedDevice();
+            var compositionGraphicsDevice = CanvasComposition.CreateCompositionGraphicsDevice(compositor, canvasDevice);
+
+            ICompositionSurface captureSurface = await compositionGraphicsDevice.CaptureAsync(
+                backingVisual,
+                new SizeInt32((int)element.RenderSize.Width, (int)element.RenderSize.Height),
+                Microsoft.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                Microsoft.Graphics.DirectX.DirectXAlphaMode.Premultiplied,
+                0);
+
+            CompositionDrawingSurface drawingSurface = captureSurface.As<CompositionDrawingSurface>();
         }
 
         public static float GetDpi(UIElement element)
