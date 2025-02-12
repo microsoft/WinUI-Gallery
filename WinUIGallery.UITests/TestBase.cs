71bb12e66857b8eecb8efa41ff07bd46ec0e9f62
Marcel W. <marcel.alex.wagner@outlook.com>
2025-02-12 14:05:41 +0100

    Update repo structure to root solution containing all projects, adjust project names (#1748)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    For easier point of entry, we move the WinUIGallery.sln and
    WinUIGalleryWAP.sln to the root and get rid of the other solution files.
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ---------
    
    Co-authored-by: Niels Laute <niels.laute@live.nl>

---
+++ WinUIGallery.UITests/TestBase.cs
@@ -0,0 +1,72 @@
+﻿//******************************************************************************
+//
+// Copyright (c) 2024 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System.Linq;
+using System.Threading;
+
+namespace WinUIGallery.UITests;
+
+public class TestBase
+{
+	public static WindowsDriver<WindowsElement> Session => SessionManager.Session;
+
+	public static void OpenControlPage(string name)
+	{
+		var search = Session.FindElementByName("Search");
+		search.Clear();
+
+		search.SendKeys(name);
+		GetElementByName(name).Click();
+
+		Assert.IsNotNull(WaitForPageHeader(name), "Failed to find matching page header for page: " + name);
+	}
+
+	public static WindowsElement GetElementByName(string name)
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			Thread.Sleep(50);
+			var element = Session.FindElementByName(name);
+			if (element != null)
+			{
+				return element;
+			}
+		}
+		return null;
+	}
+	private static WindowsElement WaitForPageHeader(string name)
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			var header = Session.FindElementByAccessibilityId("PageHeader");
+			if (header != null && header.Text == name)
+			{
+				return header;
+			}
+			Thread.Sleep(50);
+		}
+		return null;
+	}
+
+	public static void TypeText(string text)
+	{
+		var actions = new Actions(Session);
+		actions.SendKeys(text).Perform();
+	}
+}
---
+++ WinUIGallery.UITests/Tests/MediaPlayerElement.cs
@@ -0,0 +1,52 @@
+//******************************************************************************
+//
+// Copyright (c) 2024 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using System.Threading;
+
+namespace WinUIGallery.UITests.Tests;
+
+[TestClass]
+public class MediaPlayerElement : TestBase
+{
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        // Doing this manually due to keyboard layout issues surrounding y and z
+        var search = Session.FindElementByName("Search");
+        search.Clear();
+        Thread.Sleep(1_000);
+        search.SendKeys("MediaPla");
+        GetElementByName("MediaPlayerElement").Click();
+    }
+
+    [TestMethod]
+    public void PlayMedia()
+    {
+        WindowsElement play = Session.FindElementByAccessibilityId("PlayPauseButton");
+        Assert.IsNotNull(play);
+        Assert.IsNotNull(Session.FindElementByAccessibilityId("svPanel"));
+
+        // Play the video
+        play.Click();
+        Thread.Sleep(1000);
+        
+        // Pause the video
+        play.Click();
+    }
+}
