0f4ed92db304bc6435fedb41f021242030ed73a0
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-02-25 12:55:39 +0100

    Added CustomUserControls page with examples of custom controls

---
+++ WinUIGallery/Samples/ControlPages/Fundamentals/CustomUserControlsPage.xaml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace WinUIGallery.ControlPages;
+
+public sealed partial class CustomUserControlsPage : Page
+{
+    public CustomUserControlsPage()
+    {
+        this.InitializeComponent();
+    }
+}
