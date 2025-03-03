5c42ed9d9459b784200119db2520b2c0ef289d8f
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-03-03 01:25:08 +0100

    Remove the last example and update 2nd example by adding comments

--- WinUIGallery/Samples/ControlPages/Fundamentals/CustomUserControlsPage.xaml.cs
+++ WinUIGallery/Samples/ControlPages/Fundamentals/CustomUserControlsPage.xaml.cs
@@ -8,16 +8,5 @@ public sealed partial class CustomUserControlsPage : Page
     public CustomUserControlsPage()
     {
         this.InitializeComponent();
-
-        // Example synonyms dictionary
-        var synonymsDictionary = new Dictionary<string, List<string>>
-            {
-                { "happy", new List<string> { "joyful", "cheerful", "content" } },
-                { "fast", new List<string> { "quick", "speedy", "rapid" } },
-                { "smart", new List<string> { "intelligent", "clever", "bright" } }
-            };
-
-        // Set data source
-        SynonymFinder.SynonymsSource = synonymsDictionary;
     }
 }
