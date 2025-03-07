25c6b3f135efc62566d7268671053954ba52a7b5
Niels Laute <niels.laute@live.nl>
2025-03-07 15:07:53 +0100

    Move helpers out of App.xaml.cs

---
+++ WinUIGallery/Helpers/EnumHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Reflection;
+
+namespace WinUIGallery.Helpers;
+internal static class EnumHelper
+{
+    /// <summary>
+    /// Converts a string into a enum.
+    /// </summary>
+    /// <typeparam name="TEnum">The output enum type.</typeparam>
+    /// <param name="text">The input text.</param>
+    /// <returns>The parsed enum.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the TEnum type is not a enum.</exception>
+    public static TEnum GetEnum<TEnum>(string text) where TEnum : struct
+    {
+        if (!typeof(TEnum).GetTypeInfo().IsEnum)
+        {
+            throw new InvalidOperationException("Generic parameter 'TEnum' must be an enum.");
+        }
+        return (TEnum)Enum.Parse(typeof(TEnum), text);
+    }
+}
