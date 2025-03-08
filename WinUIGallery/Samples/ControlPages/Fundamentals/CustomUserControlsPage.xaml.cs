b15944897849ab132a6b04ddd2429bc4fd2c8ce5
Zakaria Tahri | Shade <101801384+Zakariathr22@users.noreply.github.com>
2025-03-08 20:44:20 +0100

    Add `Custom & User Controls` Sample (#1786)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    Added a new `Custom & User Controls` page with examples of both **Custom
    Controls** and **UserControls** to demonstrate reusable UI components.
    Main changes:
    - Added sample info to `ControlInfoData.json`.  
    - Added `Custom & User Controls Page`.  
    - Added sample controls.  
    - Added sample code files.
    
    ## Motivation and Context
    This change provides a structured way to showcase both **Custom
    Controls** and **UserControls** helping developers understand their
    differences, implementation, and use cases.
    
    Closes #1762 
    
    ## How Has This Been Tested?
    **Manually Tested**
    
    ## Screenshots (if appropriate):
    
    ![image](https://github.com/user-attachments/assets/211abb9b-1273-4567-a418-e62244d5f75e)
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ---------
    
    Co-authored-by: Niels Laute <niels.laute@live.nl>

---
+++ WinUIGallery/Samples/ControlPages/Fundamentals/CustomUserControlsPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUIGallery.ControlPages;
+
+public sealed partial class CustomUserControlsPage : Page
+{
+    public CustomUserControlsPage()
+    {
+        this.InitializeComponent();
+    }
+}
