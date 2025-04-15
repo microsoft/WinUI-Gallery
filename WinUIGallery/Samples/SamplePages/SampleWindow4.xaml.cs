7e2e91738bd234c61835bc5fa5673ca6057da131
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-04-15 17:49:28 +0100

    refactor: simplify AppWindow sample windows logic

--- WinUIGallery/Samples/SamplePages/SampleWindow4.xaml.cs
+++ WinUIGallery/Samples/SamplePages/SampleWindow4.xaml.cs
@@ -1,48 +1,38 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using WinRT.Interop;
+using System.Threading.Tasks;
 
 namespace WinUIGallery.Samples.SamplePages;
 
 public sealed partial class SampleWindow4 : Window
 {
-    private AppWindow appWindow;
-    private OverlappedPresenter presenter;
-
     public SampleWindow4(int MinWidth, int MinHeight, int MaxWidth, int MaxHeight)
     {
         this.InitializeComponent();
 
-        appWindow = GetAppWindowForCurrentWindow();
-        appWindow.Resize(new Windows.Graphics.SizeInt32(800, 500));
-        presenter = OverlappedPresenter.Create();
+        AppWindow.Resize(new Windows.Graphics.SizeInt32(800, 500));
+        AppWindow.SetIcon("Assets/Tiles/GalleryIcon.ico");
+        AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+
+        OverlappedPresenter presenter = OverlappedPresenter.Create();
         presenter.PreferredMinimumWidth = MinWidth;
         presenter.PreferredMinimumHeight = MinHeight;
         presenter.PreferredMaximumWidth = MaxWidth;
         presenter.PreferredMaximumHeight = MaxHeight;
         presenter.IsMaximizable = false;
-        appWindow.SetPresenter(presenter);
-        appWindow.SetIcon("Assets/Tiles/GalleryIcon.ico");
-        appWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
-    }
 
-    private AppWindow GetAppWindowForCurrentWindow()
-    {
-        IntPtr hWnd = WindowNative.GetWindowHandle(this);
-        WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-        return AppWindow.GetFromWindowId(myWndId);
+        AppWindow.SetPresenter(presenter);
     }
 
     private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
     {
+        OverlappedPresenter presenter = (OverlappedPresenter)AppWindow.Presenter;
         presenter.Minimize();
     }
 
     private void RestoreBtn_Click(object sender, RoutedEventArgs e)
     {
+        OverlappedPresenter presenter = (OverlappedPresenter)AppWindow.Presenter;
         presenter.Minimize();
         Task.Delay(3000).ContinueWith(t => presenter.Restore());
     }
