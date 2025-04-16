af7a3dc1668974ee51542df380fad990dcdab116
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-04-16 19:22:01 +0100

    Refactor: Simplify `AppWindow` sample windows logic (#1834)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    This PR simplifies the `AppWindow` sample windows logic by removing
    unnecessary code and making additional changes to improve readability
    and maintainability.
    
    ## How Has This Been Tested?
    Manually tested
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)

--- WinUIGallery/Samples/SamplePages/SampleWindow1.xaml.cs
+++ WinUIGallery/Samples/SamplePages/SampleWindow1.xaml.cs
@@ -1,73 +1,47 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using WinRT.Interop;
-using WinUIGallery.Helpers;
+using System.Threading.Tasks;
 
 namespace WinUIGallery.Samples.SamplePages;
 
 public sealed partial class SampleWindow1 : Window
 {
-    private AppWindow appWindow;
-
-    public SampleWindow1()
+    public SampleWindow1(string WindowTitle, int Width, int Height, int X, int Y, TitleBarTheme TitleBarPreferredTheme)
     {
         this.InitializeComponent();
-    }
-
-    public SampleWindow1(string WindowTitle, Int32 Width, Int32 Height, Int32 X, Int32 Y, TitleBarTheme TitleBarPreferredTheme)
-    {
-        this.InitializeComponent();
-
-        // Retrieve the AppWindow instance for the current window
-        appWindow = GetAppWindowForCurrentWindow();
 
         // Set the window title
-        appWindow.Title = WindowTitle;
+        AppWindow.Title = WindowTitle;
 
         // Set the window size (including borders)
-        appWindow.Resize(new Windows.Graphics.SizeInt32(Width, Height));
+        AppWindow.Resize(new Windows.Graphics.SizeInt32(Width, Height));
 
         // Set the window position on screen
-        appWindow.Move(new Windows.Graphics.PointInt32(X, Y));
+        AppWindow.Move(new Windows.Graphics.PointInt32(X, Y));
 
         // Set the preferred theme for the title bar
-        appWindow.TitleBar.PreferredTheme = TitleBarPreferredTheme;
+        AppWindow.TitleBar.PreferredTheme = TitleBarPreferredTheme;
 
         // Set the taskbar icon (displayed in the taskbar)
-        appWindow.SetTaskbarIcon("Assets/Tiles/GalleryIcon.ico");
+        AppWindow.SetTaskbarIcon("Assets/Tiles/GalleryIcon.ico");
 
         // Set the title bar icon (displayed in the window's title bar)
-        appWindow.SetTitleBarIcon("Assets/Tiles/GalleryIcon.ico");
+        AppWindow.SetTitleBarIcon("Assets/Tiles/GalleryIcon.ico");
 
         // Set the window icon (affects both taskbar and title bar, can be omitted if the above two are set)
         // appWindow.SetIcon("Assets/Tiles/GalleryIcon.ico"); 
     }
 
-    //Returrns the AppWindow instance associated with the current window.
-    private AppWindow GetAppWindowForCurrentWindow()
+    private async void Show_Click(object sender, RoutedEventArgs e)
     {
-        // Get the native window handle
-        IntPtr hWnd = WindowNative.GetWindowHandle(this);
-
-        // Retrieve the WindowId from the window handle
-        WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-
-        // Return the AppWindow instance associated with the given WindowId
-        return AppWindow.GetFromWindowId(myWndId);
-    }
-
-    private void Show_Click(object sender, RoutedEventArgs e)
-    {
-        appWindow.Hide();
-        Task.Delay(3000).ContinueWith(t => appWindow.Show());
+        AppWindow.Hide();
+        await Task.Delay(3000);
+        AppWindow.Show();
     }
 
     private void Hide_Click(object sender, RoutedEventArgs e)
     {
-        appWindow.Hide();
+        AppWindow.Hide();
     }
 
     private void Close_Click(object sender, RoutedEventArgs e)
