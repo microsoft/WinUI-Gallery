8217a2640640aa55fee311896706069826732870
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-04-18 20:06:43 +0100

    Remove logic for inserting math symbols and structures

--- WinUIGallery/Samples/ControlPages/RichEditBoxPage.xaml.cs
+++ WinUIGallery/Samples/ControlPages/RichEditBoxPage.xaml.cs
@@ -19,7 +19,8 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using System.Runtime.InteropServices;
-using WinUIGallery.Helpers;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace WinUIGallery.ControlPages;
 
@@ -28,20 +29,12 @@ public sealed partial class RichEditBoxPage : Page
     [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto, PreserveSig = true, SetLastError = false)]
     public static extern IntPtr GetActiveWindow();
     private Windows.UI.Color currentColor = Microsoft.UI.Colors.Green;
-    private List<MathSymbol> Symbols = MathModeHelper.GetSymbols();
-    private List<MathStructure> BasicStructures;
-    private List<MathStructure> Integrals;
-    private List<MathStructure> LargeOperators;
-    private List<MathStructure> Accents;
-    private List<MathStructure> LimitAndFunctions;
-    private List<MathStructure> Matrices;
 
     public RichEditBoxPage()
     {
         this.InitializeComponent();
 
         MathEditor.TextDocument.SetMathMode(RichEditMathMode.MathOnly);
-        MathSymbolsItems.ItemsSource = Symbols;
         mathEditor2.TextDocument.SetMathMode(RichEditMathMode.MathOnly);
     }
 
@@ -241,203 +234,13 @@ public sealed partial class RichEditBoxPage : Page
         }
     }
 
-    private void SymbolDataTemplate_Loading(FrameworkElement sender, object args)
-    {
-        if (sender is Grid grid)
-        {
-            int index = MathSymbolsItems.GetElementIndex(grid);
-
-            if (index % 2 == 0)
-            {
-                grid.Style = (Style)MathModeExample.Resources["ItemStyle1"];
-            }
-            else
-            {
-                grid.Style = (Style)MathModeExample.Resources["ItemStyle2"];
-            }
-        }
-    }
-
-    private void InsertSymbolBtn_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender is Button insertionBtn)
-        {
-            if (MathEditor == null) return;
-
-            MathEditor.Focus(FocusState.Programmatic);
-
-            var selection = MathEditor.Document.Selection;
-            if (selection != null)
-            {
-                InputSender.SendUnicodeText(insertionBtn.Tag.ToString());
-            }
-        }
-    }
-
-    private void SelectorBar_SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args)
-    {
-        if(sender is SelectorBar selectorBar)
-        {
-            if (selectorBar.SelectedItem.Tag == null) return;
-
-            if (selectorBar.SelectedItem.Tag.ToString() == "Symbols")
-            {
-                SymbolsTable.Visibility = Visibility.Visible;
-                StructuresTable.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                StructuresTable.Visibility = Visibility.Visible;
-                SymbolsTable.Visibility = Visibility.Collapsed;
-                switch (selectorBar.SelectedItem.Tag.ToString())
-                {
-                    case "Structures":
-                        if (BasicStructures == null)
-                        {
-                            BasicStructures = MathModeHelper.GetStructures(StructuresCategory.BasicStructures);
-                        }
-                        MathStructuresItems.ItemsSource = BasicStructures;
-                        break;
-                    case "Integrals":
-                        if (Integrals == null)
-                        {
-                            Integrals = MathModeHelper.GetStructures(StructuresCategory.Integrals);
-                        }
-                        MathStructuresItems.ItemsSource = Integrals;
-                        break;
-                    case "LargeOperators":
-                        if (LargeOperators == null)
-                        {
-                            LargeOperators = MathModeHelper.GetStructures(StructuresCategory.LargeOperators);
-                        }
-                        MathStructuresItems.ItemsSource = LargeOperators;
-                        break;
-                    case "Accents":
-                        if (Accents == null)
-                        {
-                            Accents = MathModeHelper.GetStructures(StructuresCategory.Accents);
-                        }
-                        MathStructuresItems.ItemsSource = Accents;
-                        break;
-                    case "LimitAndFunctions":
-                        if (LimitAndFunctions == null)
-                        {
-                            LimitAndFunctions = MathModeHelper.GetStructures(StructuresCategory.LimitAndFunctions);
-                        }
-                        MathStructuresItems.ItemsSource = LimitAndFunctions;
-                        break;
-                    case "Matrices":
-                        if (Matrices == null)
-                        {
-                            Matrices = MathModeHelper.GetStructures(StructuresCategory.Matrices);
-                        }
-                        MathStructuresItems.ItemsSource = Matrices;
-                        break;
-                }
-            }
-        }
-    }
-
-    private void StructureDataTemplate_Loading(FrameworkElement sender, object args)
-    {
-        if (sender is Grid grid)
-        {
-            int index = MathStructuresItems.GetElementIndex(grid);
-
-            if (index % 2 == 0)
-            {
-                grid.Style = (Style)MathModeExample.Resources["ItemStyle1"];
-            }
-            else
-            {
-                grid.Style = (Style)MathModeExample.Resources["ItemStyle2"];
-            }
-        }
-    }
-
-    private void LightThemeImage_Loaded(object sender, RoutedEventArgs e)
-    {
-        if(sender is Image image)
-        {
-            if(image.ActualTheme == ElementTheme.Light)
-            {
-                image.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                image.Visibility = Visibility.Collapsed;
-            }
-        }
-    }
-
-    private void DarkThemeImage_Loaded(object sender, RoutedEventArgs e)
-    {
-        if (sender is Image image)
-        {
-            if (image.ActualTheme == ElementTheme.Dark)
-            {
-                image.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                image.Visibility = Visibility.Collapsed;
-            }
-        }
-    }
-
-    private void LightThemeImage_ActualThemeChanged(FrameworkElement sender, object args)
-    {
-        if (sender is Image image)
-        {
-            if (image.ActualTheme == ElementTheme.Light)
-            {
-                image.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                image.Visibility = Visibility.Collapsed;
-            }
-        }
-    }
-
-    private void DarkThemeImage_ActualThemeChanged(FrameworkElement sender, object args)
-    {
-        if (sender is Image image)
-        {
-            if (image.ActualTheme == ElementTheme.Dark)
-            {
-                image.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                image.Visibility = Visibility.Collapsed;
-            }
-        }
-    }
-
-    private void InsertStructureBtn_Click(object sender, RoutedEventArgs e)
-    {
-        if (sender is Button insertionBtn)
-        {
-            if (MathEditor == null) return;
-
-            MathEditor.Focus(FocusState.Programmatic);
-
-            var selection = MathEditor.Document.Selection;
-            if (selection != null)
-            {
-                InputSender.SendUnicodeText(insertionBtn.Tag.ToString());
-            }
-        }
-    }
-
     private void mathEditor2_TextChanged(object sender, RoutedEventArgs e)
     {
         string extractedMathML;
         mathEditor2.Document.GetMathML(out extractedMathML);
         if (!string.IsNullOrEmpty(extractedMathML))
         {
-            MathmlPresenter.Code = MathModeHelper.FormatMathML(extractedMathML);
+            MathmlPresenter.Code = FormatMathML(extractedMathML);
         }
         else
         {
@@ -445,10 +248,11 @@ public sealed partial class RichEditBoxPage : Page
         }
     }
 
-    private void SetMathmlEquationBtn_Click(object sender, RoutedEventArgs e)
+    private void SetMathmlFormulaBtn_Click(object sender, RoutedEventArgs e)
     {
-        string equationMathML = "<mml:math xmlns:mml=\"http://www.w3.org/1998/Math/MathML\" display=\"block\">" +
-            "\r\n  <mml:mi mathcolor=\"#000000\">x</mml:mi>\r\n" +
+        string formulaMathML =
+            "<mml:math xmlns:mml=\"http://www.w3.org/1998/Math/MathML\" display=\"block\">\r\n" +
+            "  <mml:mi mathcolor=\"#000000\">x</mml:mi>\r\n" +
             "  <mml:mo mathcolor=\"#000000\">\u2208</mml:mo>\r\n" +
             "  <mml:mi mathcolor=\"#000000\">P</mml:mi>\r\n" +
             "  <mml:mfenced>\r\n" +
@@ -464,11 +268,25 @@ public sealed partial class RichEditBoxPage : Page
 
         if(mathEditor2.ActualTheme == ElementTheme.Dark)
         {
-            mathEditor2.Document.SetMathML(equationMathML.Replace("mathcolor=\"#000000\"", "mathcolor=\"#FFFFFF\""));
+            mathEditor2.Document.SetMathML(formulaMathML.Replace("mathcolor=\"#000000\"", "mathcolor=\"#FFFFFF\""));
         }
         else
         {
-            mathEditor2.Document.SetMathML(equationMathML.Replace("mathcolor=\"#FFFFFF\"", "mathcolor=\"#000000\""));
+            mathEditor2.Document.SetMathML(formulaMathML.Replace("mathcolor=\"#FFFFFF\"", "mathcolor=\"#000000\""));
+        }
+    }
+
+    private static string FormatMathML(string mathML)
+    {
+        try
+        {
+            XDocument doc = XDocument.Parse(mathML);
+            return doc.ToString(); // This automatically formats with proper indentation
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error formatting MathML: {ex.Message}");
+            return mathML; // Return the original in case of an error
         }
     }
 }
