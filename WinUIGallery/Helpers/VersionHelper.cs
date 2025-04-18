b01c06b866f27176191467647411c13da2179f3a
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-04-18 12:31:18 +0100

    fix: use ReleaseInfo.AsString directly since it already includes the version tag

--- WinUIGallery/Helpers/VersionHelper.cs
+++ WinUIGallery/Helpers/VersionHelper.cs
@@ -3,9 +3,8 @@ using Microsoft.Windows.ApplicationModel.WindowsAppRuntime;
 namespace WinUIGallery.Helpers;
 internal static class VersionHelper
 {
-    public static string WinAppSdkDetails =>
-        $"Windows App SDK {ReleaseInfo.AsString}" +
-        (string.IsNullOrEmpty(ReleaseInfo.VersionTag) ? "" : $" ({ReleaseInfo.VersionTag})");
+    public static string WinAppSdkDetails => 
+        $"Windows App SDK {ReleaseInfo.AsString}";
 
     public static string WinAppSdkRuntimeDetails => 
         WinAppSdkDetails + $", Windows App Runtime {RuntimeInfo.AsString}";  
