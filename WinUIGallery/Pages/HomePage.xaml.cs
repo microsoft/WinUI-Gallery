fbaba9a6ba576a3ac256436ea48a562e06259452
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2025-05-14 21:29:56 +0100

    Refactor HomePage tabs to use simplified SelectorBar layout
    - Replaced custom tab implementation with a styled SelectorBar for better accessibility and consistency.
    - Reduced tabs to only "Recent" and "Favorites".
    - "Recent" tab now includes a horizontal scroller for the last visited samples (up to 7), followed by 2 ItemRepeaters of recently added and updated a samples.
    - "Favorites" tab displays an ItemsRepeater of favorite items.
    - Removed redundant "All samples" section from homepage, aligning with navigation structure.
    - Applied visual style from AI Dev Gallery for SelectorBar to maintain design consistency.
    - Redesign item template to align the new design and prevent hovering border issue in GridView.

--- WinUIGallery/Pages/HomePage.xaml.cs
+++ WinUIGallery/Pages/HomePage.xaml.cs
@@ -10,16 +10,24 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml;
-using WinUIGallery.Controls;
 using WinUIGallery.Helpers;
 using WinUIGallery.Models;
+using Microsoft.UI.Xaml.Hosting;
+using System.Threading.Tasks;
+using System;
 
 namespace WinUIGallery.Pages;
 
 public sealed partial class HomePage : ItemsPageBase
 {
+    IReadOnlyList<ControlInfoDataItem> RecentlyVisitedSamplesList;
+    IReadOnlyList<ControlInfoDataItem> RecentlyAddedSamplesList;
+    IReadOnlyList<ControlInfoDataItem> RecentlyUpdatedSamplesList;
+    IReadOnlyList<ControlInfoDataItem> FavoriteSamplesList;
+
     public HomePage()
     {
         this.InitializeComponent();
@@ -30,68 +38,39 @@ public sealed partial class HomePage : ItemsPageBase
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
         NavigationRootPageArgs args = (NavigationRootPageArgs)e.Parameter;
-        var menuItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.NavigationRootPage.NavigationView.MenuItems.First();
+        var menuItem = (NavigationViewItem)args.NavigationRootPage.NavigationView.MenuItems.First();
         menuItem.IsSelected = true;
 
         Items = ControlInfoDataSource.Instance.Groups
             .SelectMany(g => g.Items)
             .OrderBy(i => i.Title)
             .ToList();
-    }
 
-    private void SampleFilterBar_SelectionChanged(object sender, AdaptiveSelectorBarItem e)
-    {
-        if (e == null || e.Tag == null)
-            return;
+        RecentlyVisitedSamplesList = GetValidItems(SettingsKeys.RecentlyVisited);
+        RecentlyAddedSamplesList = Items.Where(i => i.IsNew).ToList();
+        RecentlyUpdatedSamplesList = Items.Where(i => i.IsUpdated && !i.IsNew).ToList();
+        FavoriteSamplesList = GetValidItems(SettingsKeys.Favorites);
 
-        string selectedTag = e.Tag.ToString();
-        List<ControlInfoDataItem> filteredItems = [];
-
-        switch (selectedTag)
+        if (RecentlyVisitedSamplesList.Count > 0)
         {
-            case "RecentlyAdded":
-                filteredItems = Items.Where(i => i.IsNew).ToList();
-                break;
-            case "RecentlyUpdated":
-                filteredItems = Items.Where(i => i.IsUpdated && !i.IsNew).ToList();
-                break;
-            case "RecentlyVisited":
-                filteredItems = GetValidItems(SettingsKeys.RecentlyVisited);
-                break;
-            case "FavoriteSamples":
-                filteredItems = GetValidItems(SettingsKeys.Favorites);
-                break;
-            case "PreviewSamples":
-                filteredItems = Items.Where(i => i.IsNew).ToList();
-                break;
-            case "AllSamples":
-                filteredItems = (List<ControlInfoDataItem>)Items;
-                break;
-            default:
-                Debug.WriteLine("Unknown filter tag.");
-                break;
-        }
-
-        itemsRepeater.ItemsSource = filteredItems;
-
-        if (filteredItems.Count == 0 && (selectedTag == "RecentlyVisited" || selectedTag == "FavoriteSamples"))
-        {
-            fallBackMessage.Visibility = Visibility.Visible;
-
-            if (selectedTag == "RecentlyVisited")
-            {
-                fallBackMessageTitle.Text = "No recently visited samples.";
-                fallBackMessageSubtitle.Text = "Samples you view will show up here.";
-            }
-            else if (selectedTag == "FavoriteSamples")
-            {
-                fallBackMessageTitle.Text = "No favorite samples yet.";
-                fallBackMessageSubtitle.Text = "Mark samples as favorites to see them here.";
-            }
+            RecentlyVisitedTiltle.Visibility = Visibility.Visible;
+            RecentlyVisitedContainer.Visibility = Visibility.Visible;
         }
         else
         {
-            fallBackMessage.Visibility = Visibility.Collapsed;
+            RecentlyVisitedTiltle.Visibility = Visibility.Collapsed;
+            RecentlyVisitedContainer.Visibility = Visibility.Collapsed;
+        }
+
+        if (FavoriteSamplesList.Count > 0)
+        {
+            FavoriteSamplesFallbackMessage.Visibility = Visibility.Collapsed;
+            FavoriteSamples.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            FavoriteSamplesFallbackMessage.Visibility = Visibility.Visible;
+            FavoriteSamples.Visibility = Visibility.Collapsed;
         }
     }
 
@@ -119,5 +98,61 @@ public sealed partial class HomePage : ItemsPageBase
         }
 
         return result;
+    }
+
+    private async void CategorySelectorBar_SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args)
+    {
+        if (sender is not SelectorBar selectorBar) return;
+        if (selectorBar.SelectedItem is not SelectorBarItem item) return;
+        if (item.Tag is not string tag) return;
+
+        switch (tag)
+        {
+            case "Recent":
+                await FavoriteSamplesPanel.FadeOut();
+                await RecentSamplesPanel.FadeIn();
+                break;
+            case "Favorites":
+                await RecentSamplesPanel.FadeOut();
+                await FavoriteSamplesPanel.FadeIn();
+                break;
+            default:
+                Debug.Assert(false, $"Unknown tag: {tag}");
+                break;
+        }
+    }
+}
+
+public static class UIElementExtensions
+{
+    public static async Task FadeIn(this UIElement element, double duration = 150)
+    {
+        var visual = ElementCompositionPreview.GetElementVisual(element);
+        var compositor = visual.Compositor;
+
+        var fadeIn = compositor.CreateScalarKeyFrameAnimation();
+        fadeIn.InsertKeyFrame(1f, 1f);
+        fadeIn.Duration = TimeSpan.FromMilliseconds(duration);
+
+        element.Visibility = Visibility.Visible;
+        visual.Opacity = 0;
+        visual.StartAnimation("Opacity", fadeIn);
+
+        await Task.Delay((int)duration);
+    }
+
+    public static async Task FadeOut(this UIElement element, double duration = 150)
+    {
+        var visual = ElementCompositionPreview.GetElementVisual(element);
+        var compositor = visual.Compositor;
+
+        var fadeOut = compositor.CreateScalarKeyFrameAnimation();
+        fadeOut.InsertKeyFrame(1f, 0f);
+        fadeOut.Duration = TimeSpan.FromMilliseconds(duration);
+
+        visual.StartAnimation("Opacity", fadeOut);
+        await Task.Delay((int)duration);
+
+        element.Visibility = Visibility.Collapsed;
     }
 }
