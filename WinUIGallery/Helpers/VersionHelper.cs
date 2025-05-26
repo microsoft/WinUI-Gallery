114ea78c997d5cbbb2332f9cd00a570ad70db443
Niels Laute <niels.laute@live.nl>
2025-05-26 15:03:44 +0200

    Update VersionHelper.cs

--- WinUIGallery/Helpers/VersionHelper.cs
+++ WinUIGallery/Helpers/VersionHelper.cs
@@ -4,7 +4,7 @@ namespace WinUIGallery.Helpers;
 internal static class VersionHelper
 {
     public static string WinAppSdkDetails => 
-        $"Windows App SDK {ReleaseInfo.Major}.{ReleaseInfo.Minor}.{ReleaseInfo.Patch}";
+        $"Windows App SDK {ReleaseInfo.Major}.{ReleaseInfo.Minor}";
 
     public static string WinAppSdkRuntimeDetails => 
         WinAppSdkDetails + $", Windows App Runtime {RuntimeInfo.AsString}";  
