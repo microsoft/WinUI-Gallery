e4d09703fc7b2e1e4e58a5c838a0eae2076f8e32
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-07-13 00:29:18 +0330

    Fix PersonPicture Sample

--- WinUIGallery/Converters/BooleanToValueConverter.cs
+++ WinUIGallery/Converters/BooleanToValueConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 
 namespace WinUIGallery.Converters;
@@ -10,7 +11,26 @@ public sealed partial class BooleanToValueConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, string language)
     {
-        return (bool)value ? parameter : null;
+        if (value is bool isChecked && isChecked)
+        {
+            if (parameter is string str)
+            {
+                // Check if it's a URL (basic check)
+                if (str.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ||
+                    str.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+                {
+                    return new BitmapImage(new Uri(str));
+                }
+
+                // Return the string directly if not a URL (for DisplayName or Initials)
+                return str;
+            }
+
+            return parameter;
+        }
+
+        // If not checked, return null (clears DisplayName, Initials, or Image)
+        return null;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, string language)
