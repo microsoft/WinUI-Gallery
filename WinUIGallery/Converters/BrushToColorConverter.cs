e1fa6b677f061d53bbaa19feba648dcdb12c0499
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-07-26 15:55:26 +0330

    Fix CopyButton AOT Warning

---
+++ WinUIGallery/Converters/BrushToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace WinUIGallery.Converters;
+public partial class BrushToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is SolidColorBrush solidColorBrush)
+        {
+            return solidColorBrush.Color;
+        }
+
+        return Colors.Transparent;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
