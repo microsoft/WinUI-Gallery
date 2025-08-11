7f04f89a002670138e2ef9c14db25e13908cc5fb
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-08-11 20:26:11 +0330

    Remove Unsued Vector Wrapper

--- WinUIGallery/Helpers/SettingsHelper/Internals/SettingsJsonContext.cs
+++ WinUIGallery/Helpers/SettingsHelper/Internals/SettingsJsonContext.cs
@@ -5,8 +5,6 @@ namespace WinUIGallery.Helpers;
 
 [JsonSourceGenerationOptions(WriteIndented = true, GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(Dictionary<string, System.Text.Json.JsonElement>))]
-[JsonSerializable(typeof(Vector2Data))]
-[JsonSerializable(typeof(Vector3Data))]
 [JsonSerializable(typeof(SettingsHelper))]
 internal partial class SettingsJsonContext : JsonSerializerContext
 {
