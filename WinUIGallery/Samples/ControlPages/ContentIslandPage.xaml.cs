2c82c3e8377c4e2debcff674973a594dcd6fd66b
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2026-04-04 05:18:12 +0900

    Fix ContentIsland page leak (#2152)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    This PR will improve `ContentIsland` lifecycle and resource management.
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    Partial fix for #2149.
    
    ## How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    Tested manually.
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)

--- WinUIGallery/Samples/ControlPages/ContentIslandPage.xaml.cs
+++ WinUIGallery/Samples/ControlPages/ContentIslandPage.xaml.cs
@@ -8,30 +8,52 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Shapes;
 using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Threading.Tasks;
 
 namespace WinUIGallery.ControlPages;
 
 public sealed partial class ContentIslandPage : Page
 {
+    private sealed class IslandEntry(ChildSiteLink childSiteLink, Rectangle rect, EventHandler<object?> layoutUpdatedHandler)
+    {
+        public ChildSiteLink ChildSiteLink { get; } = childSiteLink;
+
+        public ContentIsland? HelmetIsland { get; set; }
+
+        public Rectangle Rect { get; } = rect;
+
+        public EventHandler<object?> LayoutUpdatedHandler { get; } = layoutUpdatedHandler;
+    }
+
+    private readonly List<IslandEntry> _islandEntries = [];
+
     public ContentIslandPage()
     {
         this.InitializeComponent();
+        Unloaded += OnUnloaded;
     }
 
-    int idx = 0;
+    private int NextHostElementIndex { get; set; } = 0;
 
-    Rectangle? GetNextHostElement()
+    private async void LoadModel_Click(object sender, RoutedEventArgs e)
     {
-        if (idx < _rectanglePanel.Children.Count)
+        _rectanglePanel.Visibility = Visibility.Visible;
+        await LoadModel();
+    }
+
+    private Rectangle? GetNextHostElement()
+    {
+        if (NextHostElementIndex < _rectanglePanel.Children.Count)
         {
-            return ((Rectangle)_rectanglePanel.Children[idx++]);
+            return ((Rectangle)_rectanglePanel.Children[NextHostElementIndex++]);
         }
 
         return null;
     }
 
-    public async void LoadModel()
+    public async Task LoadModel()
     {
         ContentIsland parentIsland = this.XamlRoot.ContentIsland;
 
@@ -68,17 +90,38 @@ public sealed partial class ContentIslandPage : Page
         placementVisual.Size = size;
         childSiteLink.ActualSize = size;
 
+        // Track the entry before the await so OnUnloaded can dispose it even if the page
+        // is navigated away while the island is still loading.
+        var entry = new IslandEntry(childSiteLink, rect, layoutUpdatedEventHandler);
+        _islandEntries.Add(entry);
+
         // The ContentIsland is connected to the ChildSiteLink and must outlive this method.
 #pragma warning disable CA2000 // Dispose objects before losing scope
         ContentIsland helmetIsland = await HelmetScenario.CreateIsland(placementVisual.Compositor);
 #pragma warning restore CA2000
 
-        childSiteLink.Connect(helmetIsland);
+        if (!childSiteLink.IsClosed)
+        {
+            entry.HelmetIsland = helmetIsland;
+            childSiteLink.Connect(helmetIsland);
+        }
+        else
+        {
+            // The page was unloaded while the island was loading; release it immediately.
+            helmetIsland.Dispose();
+        }
     }
 
-    private void LoadModel_Click(object sender, RoutedEventArgs e)
+    private void OnUnloaded(object sender, RoutedEventArgs e)
     {
-        _rectanglePanel.Visibility = Visibility.Visible;
-        LoadModel();
+        foreach (IslandEntry entry in _islandEntries)
+        {
+            entry.Rect.LayoutUpdated -= entry.LayoutUpdatedHandler;
+            entry.ChildSiteLink.Dispose();
+            entry.HelmetIsland?.Dispose();
+        }
+
+        _islandEntries.Clear();
+        NextHostElementIndex = 0;
     }
 }
